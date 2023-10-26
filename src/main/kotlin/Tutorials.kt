cf5f39faff0f091464c2a49e6d8363491e734681
Romualdus Aldi Hermanto <romheraldi.dev@gmail.com>
2023-10-26 11:21:34 +0700

    Variable and Aritmathic

--- src/main/kotlin/Tutorials.kt
+++ src/main/kotlin/Tutorials.kt
@@ -1,4 +1,8 @@
 fun main() {
-     println("Hello Guys")
-     println("Hello World")
+     var myName: String = "Romualdus Aldi Hermanto"
+     var myAge: Int = 25
+
+     var addYears = 25
+
+     println("Hello! my name is $myName, and i'm $myAge years old, $addYears years later is ${myAge + addYears}")
 }
